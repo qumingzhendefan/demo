4be5ce7884b8b8067bcc9a5e695e80daa1b4244d
tanghongzhi <tanghongzhi01@163.com>
2018-08-29 14:32:42 +0800

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * spring boot 启动类
+ */
 @SpringBootApplication
 public class DemoApplication {
 
